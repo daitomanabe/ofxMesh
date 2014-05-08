9fe73031728c6c990161e5dfce3cdb9febb7f86c
daitomanabe <daito@rhizomatiks.com>
2014-05-09 02:33:53 +0900

    addsmall functions

--- ofxMesh.cpp
+++ ofxMesh.cpp
@@ -7,6 +7,43 @@
 
 #include "ofxMesh.h"
 
+void ofxMesh::fromMesh(const ofMesh & mesh){
+    if (mesh.hasVertices()) {
+        getVertices()=mesh.getVertices();
+    }
+    if (mesh.hasColors()) {
+        getColors()=mesh.getColors();
+    }
+    if (mesh.hasNormals()) {
+        getNormals()=mesh.getNormals();
+    }
+    if (mesh.hasTexCoords()) {
+        getTexCoords()=mesh.getTexCoords();
+    }
+    if (mesh.hasIndices()) {
+        getIndices()=mesh.getIndices();
+    }
+}
+
+void ofxMesh::toMesh(ofMesh & mesh){
+    mesh.clear();
+    if (hasVertices()) {
+        mesh.getVertices()=getVertices();
+    }
+    if (hasColors()) {
+        mesh.getColors()=getColors();
+    }
+    if (hasNormals()) {
+        mesh.getNormals()=getNormals();
+    }
+    if (hasTexCoords()) {
+        mesh.getTexCoords()=getTexCoords();
+    }
+    if (hasIndices()) {
+        mesh.getIndices()=getIndices();
+    }
+}
+
 void ofxMesh::addFace(ofVec3f a, ofVec3f b, ofVec3f c) {
     ofVec3f normal = ((c - a).cross(b - a)).normalize();
     
@@ -66,9 +103,6 @@ void ofxMesh::translate(const ofVec3f & pos) {
 }
 
 void ofxMesh::translate(float x, float y, float z) {
-//    for (int i=0; i<getNumVertices(); i++) {
-//        getVertices()[i] += ofVec3f(x,y,z);
-//    }
     for (int i=0; i<getNumVertices(); i++) {
         ofVec3f tmp = getVertices()[i] + ofVec3f(x,y,z);
 		getVertices()[i].set(tmp);
@@ -101,28 +135,90 @@ void ofxMesh::invertNormals() {
     }
 }
 
+ofVec3f ofxMesh::getNormal(const ofVec3f& v1, const ofVec3f& v2, const ofVec3f& v3) {
+	ofVec3f a = v1 - v2;
+	ofVec3f b = v3 - v2;
+	ofVec3f normal = b.cross(a);
+	normal.normalize();
+	return normal;
+}
+
+void ofxMesh::buildNormals() {
+	vector<ofVec3f>& vertices = getVertices();
+	for(int i = 0; i < getNumVertices(); i += 3) {
+		ofVec3f normal = getNormal(
+                                   getVertices()[i+0],
+                                   getVertices()[i+1],
+                                   getVertices()[i+2]);
+		for(int j = 0; j < 3; j++) {
+			addNormal(normal);
+		}
+	}
+}
+
 ofxMesh &ofxMesh::addMesh(ofMesh b) {
-    
-    int numVertices = getNumVertices();    
+
+    int numVertices = getNumVertices();
     int numIndices = getNumIndices();
-    
+
     //add b
     addVertices(b.getVertices());
     addNormals(b.getNormals());
     addIndices(b.getIndices());
-    
+
     //shift indices for b
     for (int i=0; i<b.getNumIndices(); i++) {
         getIndices()[numIndices+i] += numVertices;
     }
-    
+
     return *this;
 }
 
-//ofVec3f ofxMesh::getSize() {
-//    for (int i=0; i<getNumVertices(); i++) {
-//        getVertices()[i] += pos;
-//    }
+void ofxMesh::addMeshes(const vector<ofxMesh> & input)
+{
+    for (int i=0; i<input.size(); i++) {
+        addMesh(input[i]);
+    }
+}
+
+
+//ofMesh ofxMesh::dodecahedron(float r )
+//{
+//    ofMesh mesh;
+//    vector<ofVec3f> vertices;
+//    float phi = (float) (sqrt(5) - 1) / 2; // The golden rati
+//    float R = (float) (r / sqrt(3));
 //
+//    float a = (R * 1);
+//    float b = R / phi;
+//    float c = R * phi;
+//
+//    for (int i = -1; i < 2; i += 2)
+//    {
+//        for (int j = -1; j < 2; j += 2)
+//        {
+//            mesh.addVertex(ofVec3f(
+//                                   0,
+//                                   i * c * R,
+//                                   j * b * R));
+//            mesh.addVertex(ofVec3f(
+//                                   i * c * R,
+//                                   j * b * R,
+//                                   0));
+//            mesh.addVertex(ofVec3f(
+//                                   i * b * R,
+//                                   0,
+//                                   j * c * R));
+//
+//            for (int k = -1; k < 2; k += 2){
+//
+//                mesh.addVertex(ofVec3f(
+//                                       i * a * R,
+//                                       j * a * R,
+//                                       k * a * R));
+//            }
+//        }
+//    }
+//    return mesh;
 //}
 
--- ofxMesh.h
+++ ofxMesh.h
@@ -11,42 +11,8 @@
 
 class ofxMesh : public ofMesh {
 public:  
-    void toMesh(ofMesh & mesh){
-        mesh.clear();
-		if (hasVertices()) {
-			mesh.getVertices()=getVertices();
-		}
-		if (hasColors()) {
-			mesh.getColors()=getColors();
-		}
-		if (hasNormals()) {
-			mesh.getNormals()=getNormals();
-		}
-		if (hasTexCoords()) {
-			mesh.getTexCoords()=getTexCoords();
-		}
-		if (hasIndices()) {
-			mesh.getIndices()=getIndices();
-		}
-    }
-	void fromMesh(const ofMesh & mesh){
-		if (mesh.hasVertices()) {
-			getVertices()=mesh.getVertices();
-		}
-		if (mesh.hasColors()) {
-			getColors()=mesh.getColors();
-		}
-		if (mesh.hasNormals()) {
-			getNormals()=mesh.getNormals();
-		}
-		if (mesh.hasTexCoords()) {
-			getTexCoords()=mesh.getTexCoords();
-		}
-		if (mesh.hasIndices()) {
-			getIndices()=mesh.getIndices();
-		}
-		
-	}
+    void toMesh(ofMesh & mesh);
+	void fromMesh(const ofMesh & mesh);
     void addFace(ofVec3f a, ofVec3f b, ofVec3f c);
     void addFace(ofVec3f a, ofVec3f b, ofVec3f c, ofVec3f d);
     void addFace(ofRectangle r);
@@ -58,28 +24,30 @@ public:
 	
     void scale(float x, float y, float z=1);
     void invertNormals();
-	float getWidth(){
-		
-		float minX = std::numeric_limits<int>::max();
-		float maxX = std::numeric_limits<int>::min();
-		
-		for (int i=0; i<getNumVertices(); i++) {
-			ofVec3f v = getVertices()[i];
-			minX = MIN(minX, v.x);
-			maxX = MAX(maxX, v.x);
-		}
-		float width = fabs(minX - maxX);
-		return width;
-	}
-    
-//    ofVec3f getSize();
-//    void unifyVertices();
+    ofVec3f getNormal(const ofVec3f& v1, const ofVec3f& v2, const ofVec3f& v3);
 
+    void buildNormals();
+    
     ofxMesh &addMesh(ofMesh b);
-	
-	void addMeshes(vector<ofxMesh> & input){
-		for (int i=0; i<input.size(); i++) {
-			addMesh(input[i]);
-		}
-	}
+	void addMeshes(const vector<ofxMesh> & input);
+
+//	float getWidth(){
+//
+//		float minX = std::numeric_limits<int>::max();
+//		float maxX = std::numeric_limits<int>::min();
+//
+//		for (int i=0; i<getNumVertices(); i++) {
+//			ofVec3f v = getVertices()[i];
+//			minX = MIN(minX, v.x);
+//			maxX = MAX(maxX, v.x);
+//		}
+//		float width = fabs(minX - maxX);
+//		return width;
+//	}
+    //    P(± r/√3, ± r/√3, ± r/√3)
+    //    P(0, ± r/(√3*φ), ± (r*φ)/√3)
+    //    P(± r/(√3*φ), ± (r*φ)/√3, 0)
+    //    P(± (r*φ)/√3, 0, ± r/(√3*φ))
+
+//	static ofMesh dodecahedron(float r = 1.);
 };
